6d2f0712243d03355f77567ab0378b574a92a032
shraddhakadaba123 <shraddhakadaba123@gmail.com>
2022-12-05 08:42:44 -0800

    calculator

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/Calculator.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/Calculator.java
@@ -1,3 +1,4 @@
+
 package com.nighthawk.spring_portfolio.mvc.calculator;
 
 import java.util.ArrayList;
@@ -24,11 +25,13 @@ public class Calculator {
     private final Map<String, Integer> OPERATORS = new HashMap<>();
     {
         // Map<"token", precedence>
+        OPERATORS.put("^", 2);             // inputting all of the operators
         OPERATORS.put("*", 3);
         OPERATORS.put("/", 3);
         OPERATORS.put("%", 3);
         OPERATORS.put("+", 4);
         OPERATORS.put("-", 4);
+       
     }
 
     // Helper definition for supported operators
@@ -42,17 +45,33 @@ public class Calculator {
 
     // Create a 1 argument constructor expecting a mathematical expression
     public Calculator(String expression) {
-        // original input
-        this.expression = expression;
+        
+        this.expression = expression;            // original input
+        
+        this.parenthesesCheck();                // parentheses imbalance check
 
-        // parse expression into terms
-        this.termTokenizer();
+        this.termTokenizer();                    // dividing expression into terms
 
-        // place terms into reverse polish notation
-        this.tokensToReversePolishNotation();
+        this.tokensToReversePolishNotation();     // put terms into reverse polish notation
 
-        // calculate reverse polish notation
-        this.rpnToResult();
+        this.rpnToResult();                       // calculate the reverse polish notation
+
+    }
+
+    private void parenthesesCheck() {
+        int leftParentheses = 0;
+        int rightParentheses = 0;
+        for (int i = 0; i < this.expression.length(); i++) {
+            if (this.expression.charAt(i) == '(') {
+                leftParentheses++;
+            } else if (this.expression.charAt(i) == ')') {
+                rightParentheses++;
+            }
+        }
+
+        if (leftParentheses != rightParentheses) {
+            throw new RuntimeException("Parentheses error, make sure your parentheses are balanced.");
+        }
     }
 
     // Test if token is an operator
@@ -132,6 +151,7 @@ public class Calculator {
                 case "*":
                 case "/":
                 case "%":
+                case "^":
                     // While stack
                     // not empty AND stack top element
                     // and is an operator
@@ -170,9 +190,38 @@ public class Calculator {
             if (isOperator(token))
             {
                 // Pop the two top entries
+                double a = calcStack.pop();
+                double b = calcStack.pop();
 
                 // Calculate intermediate results
-                result = 0.0;
+                switch (token) {                   // switch statements depending on what is inputed
+                    case "+":                      // executes addition
+                        result = b + a; 
+                        break; 
+                    
+                    case "-":                   // executes substraction
+                        result = b - a;
+                        break;
+                    
+                    case "*":                    // executes mutliplication
+                        result = b * a; 
+                        break; 
+                    
+                    case "/":                     // executes division
+                        result = b / a; 
+                        break; 
+                        
+                    case "%":                     // executes remainder
+                        result = b % a; 
+                        break; 
+                    
+                    case "^":
+                        result = Math.pow(b,a);
+                        break;
+
+                    default: 
+                        break;
+                }
 
                 // Push intermediate result back onto the stack
                 calcStack.push( result );
@@ -208,18 +257,45 @@ public class Calculator {
 
         System.out.println();
 
-        Calculator decimalMath = new Calculator("100.2 - 99.3");
+        Calculator decimalMath = new Calculator("100.4 - 99.8");
         System.out.println("Decimal Math\n" + decimalMath);
 
         System.out.println();
 
-        Calculator moduloMath = new Calculator("300 % 200");
+        Calculator moduloMath = new Calculator("400 % 200");
         System.out.println("Modulo Math\n" + moduloMath);
 
         System.out.println();
 
-        Calculator divisionMath = new Calculator("300/200");
+        Calculator divisionMath = new Calculator("400/200");
         System.out.println("Division Math\n" + divisionMath);
 
+        System.out.println();
+
+        Calculator powerMath = new Calculator("3^6");
+        System.out.println("Power Math\n" + powerMath);
+
+        System.out.println();
+
+        System.out.println("Parentheses imbalance error:");
+        Calculator parenthesesError = new Calculator("((100+200)*3");
+        
     }
+
+    public String calcToString(boolean x) {
+        if (x) {
+        System.out.println("--------");
+        System.out.println("Result: " + this.expression + " = " + this.result);
+        System.out.println("Tokens: " + this.tokens + " , RPN: " + this.reverse_polish);
+        }
+
+        String output = this.expression + " = " + this.result;
+        return output;
+    }
+
+    public String convertToJson() {
+        String json = "{ \"Expression\": \"" + this.expression + "\", \"Tokens\": \"" + this.tokens + "\", \"RPN\": \"" + this.reverse_polish + "\", \"Result\": " + this.result + " }";
+        return json;
+    }
+
 }
